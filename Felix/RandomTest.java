7346f828ce128c7b2343a5ace8b50f98ebfdd489
Fellan12 <fellan12@gmail.com>
2017-02-09 17:06:25 +0100

    randomtesting done

--- Felix/RandomTest.java
+++ Felix/RandomTest.java
@@ -7,6 +7,7 @@ import java.lang.StringBuilder;
 public class RandomTest{
 	static final String ANSI_RED = "\u001B[31m";
 	static final String ANSI_GREEN = "\u001B[32m";
+	static final String ANSI_RESET = "\u001B[0m";
 	static int[][] tests;
 	static int[] keys;
 	static int numOfTests = 100;
@@ -15,7 +16,7 @@ public class RandomTest{
 	public static void main(String[] args) {
 		tests = new int[numOfTests][sizeOfArray];
 		keys = new int[numOfTests];
-		String filename = "Random-tests-"+numOfTests+"-"+sizeOfArray+"-"+numberRange+".txt";
+		String filename = "Random-tests-"+numOfTests+"-"+sizeOfArray+".txt";
 
 		randomGenTests(numOfTests,sizeOfArray,numberRange,filename);
 		System.out.println("Generated " + numOfTests+ " tests");
@@ -25,6 +26,13 @@ public class RandomTest{
 		System.out.println();
 		System.out.println("Running test....");
 		runTest();
+		runTest1();
+		runTest2();
+		runTest3();
+		runTest4();
+		runTest5();
+		runTest6();
+
 	}
 
 	public static void randomGenTests(int numOfTests, int sizeOfArray, int numberRange, String filename){
@@ -54,7 +62,6 @@ public class RandomTest{
 				for(String x : testArray){								//Convert the Array to string for writing
 					sb.append(x);
 				}
-				System.out.println(key + " " + sb.toString().trim() + " " +keyInArray);
 				writer.println(key + " " + sb.toString().trim() + " " +keyInArray);	//Write the testcase to file
 			}
 
@@ -66,8 +73,10 @@ public class RandomTest{
 
 	public static void runTest(){
 		Algorithm alg = new Algorithm();
+		int wrongCounter = 0;
+		int rightCounter = 0;
 		try{
-			Scanner reader = new Scanner(new File("Random-tests-100-20-20.txt"));
+			Scanner reader = new Scanner(new File("Random-tests-100-20.txt"));
 			int numOfTests = reader.nextInt();
 			int arraySize = reader.nextInt();
 			int key, result;
@@ -78,16 +87,247 @@ public class RandomTest{
 					testArray[j] = reader.nextInt();
 				}
 				result = reader.nextInt();
-				int testRes = alg.myContains(testArray,key);
-
-				if(testRes == result){
-					System.out.println(ANSI_GREEN+"Pass " + i);
-				}else{
-					System.out.println(ANSI_RED+"Fail" + i);
+				try{
+					int testRes = alg.myContains(testArray,key);
+					if(testRes == result){
+						//System.out.println(ANSI_GREEN+"Pass " + (i+1) +ANSI_RESET);
+						rightCounter++;
+					}else{
+						//System.out.println(ANSI_RED+"Fail " + (i+1) + ": Wrong answer"+ANSI_RESET);
+						wrongCounter++;
+					}
+				}catch(Exception a){
+					//System.out.println(ANSI_RED+"Fail " + (i+1) + ": Exception"+ANSI_RESET);
+					wrongCounter++;
 				}
 			}
 		}catch(FileNotFoundException e){
 			e.printStackTrace();
 		}
+
+		System.out.println("myContains: Wrong on " + wrongCounter + " out of " + (wrongCounter+rightCounter));
 	}
+
+	public static void runTest1(){
+		Algorithm alg = new Algorithm();
+		int wrongCounter = 0;
+		int rightCounter = 0;
+		try{
+			Scanner reader = new Scanner(new File("Random-tests-100-20.txt"));
+			int numOfTests = reader.nextInt();
+			int arraySize = reader.nextInt();
+			int key, result;
+			for(int i = 0; i < numOfTests; i++){
+				key = reader.nextInt();
+				int[] testArray = new int[arraySize];
+				for(int j = 0; j < arraySize; j++){
+					testArray[j] = reader.nextInt();
+				}
+				result = reader.nextInt();
+				try{
+					int testRes = alg.myContains1(testArray,key);
+					if(testRes == result){
+						//System.out.println(ANSI_GREEN+"Pass " + (i+1) +ANSI_RESET);
+						rightCounter++;
+					}else{
+						//System.out.println(ANSI_RED+"Fail " + (i+1) + ": Wrong answer"+ANSI_RESET);
+						wrongCounter++;
+					}
+				}catch(Exception a){
+					//System.out.println(ANSI_RED+"Fail " + (i+1) + ": Exception"+ANSI_RESET);
+					wrongCounter++;
+				}
+			}
+		}catch(FileNotFoundException e){
+			e.printStackTrace();
+		}
+
+		System.out.println("myContains1: Wrong on " + wrongCounter + " out of " + (wrongCounter+rightCounter));
+	}
+
+	public static void runTest2(){
+		Algorithm alg = new Algorithm();
+		int wrongCounter = 0;
+		int rightCounter = 0;
+		try{
+			Scanner reader = new Scanner(new File("Random-tests-100-20.txt"));
+			int numOfTests = reader.nextInt();
+			int arraySize = reader.nextInt();
+			int key, result;
+			for(int i = 0; i < numOfTests; i++){
+				key = reader.nextInt();
+				int[] testArray = new int[arraySize];
+				for(int j = 0; j < arraySize; j++){
+					testArray[j] = reader.nextInt();
+				}
+				result = reader.nextInt();
+				try{
+					int testRes = alg.myContains2(testArray,key);
+					if(testRes == result){
+						//System.out.println(ANSI_GREEN+"Pass " + (i+1) +ANSI_RESET);
+						rightCounter++;
+					}else{
+						//System.out.println(ANSI_RED+"Fail " + (i+1) + ": Wrong answer"+ANSI_RESET);
+						wrongCounter++;
+					}
+				}catch(Exception a){
+					//System.out.println(ANSI_RED+"Fail " + (i+1) + ": Exception"+ANSI_RESET);
+					wrongCounter++;
+				}
+			}
+		}catch(FileNotFoundException e){
+			e.printStackTrace();
+		}
+
+		System.out.println("myContains2: Wrong on " + wrongCounter + " out of " + (wrongCounter+rightCounter));
+	}
+
+	public static void runTest3(){
+		Algorithm alg = new Algorithm();
+		int wrongCounter = 0;
+		int rightCounter = 0;
+		try{
+			Scanner reader = new Scanner(new File("Random-tests-100-20.txt"));
+			int numOfTests = reader.nextInt();
+			int arraySize = reader.nextInt();
+			int key, result;
+			for(int i = 0; i < numOfTests; i++){
+				key = reader.nextInt();
+				int[] testArray = new int[arraySize];
+				for(int j = 0; j < arraySize; j++){
+					testArray[j] = reader.nextInt();
+				}
+				result = reader.nextInt();
+				try{
+					int testRes = alg.myContains3(testArray,key);
+					if(testRes == result){
+						//System.out.println(ANSI_GREEN+"Pass " + (i+1) +ANSI_RESET);
+						rightCounter++;
+					}else{
+						//System.out.println(ANSI_RED+"Fail " + (i+1) + ": Wrong answer"+ANSI_RESET);
+						wrongCounter++;
+					}
+				}catch(Exception a){
+					//System.out.println(ANSI_RED+"Fail " + (i+1) + ": Exception"+ANSI_RESET);
+					wrongCounter++;
+				}
+			}
+		}catch(FileNotFoundException e){
+			e.printStackTrace();
+		}
+
+		System.out.println("myContains3: Wrong on " + wrongCounter + " out of " + (wrongCounter+rightCounter));
+	}
+
+	public static void runTest4(){
+		Algorithm alg = new Algorithm();
+		int wrongCounter = 0;
+		int rightCounter = 0;
+		try{
+			Scanner reader = new Scanner(new File("Random-tests-100-20.txt"));
+			int numOfTests = reader.nextInt();
+			int arraySize = reader.nextInt();
+			int key, result;
+			for(int i = 0; i < numOfTests; i++){
+				key = reader.nextInt();
+				int[] testArray = new int[arraySize];
+				for(int j = 0; j < arraySize; j++){
+					testArray[j] = reader.nextInt();
+				}
+				result = reader.nextInt();
+				try{
+					int testRes = alg.myContains4(testArray,key);
+					if(testRes == result){
+						//System.out.println(ANSI_GREEN+"Pass " + (i+1) +ANSI_RESET);
+						rightCounter++;
+					}else{
+						//System.out.println(ANSI_RED+"Fail " + (i+1) + ": Wrong answer"+ANSI_RESET);
+						wrongCounter++;
+					}
+				}catch(Exception a){
+					//System.out.println(ANSI_RED+"Fail " + (i+1) + ": Exception"+ANSI_RESET);
+					wrongCounter++;
+				}
+			}
+		}catch(FileNotFoundException e){
+			e.printStackTrace();
+		}
+
+		System.out.println("myContains4: Wrong on " + wrongCounter + " out of " + (wrongCounter+rightCounter));
+	}
+
+	public static void runTest5(){
+		Algorithm alg = new Algorithm();
+		int wrongCounter = 0;
+		int rightCounter = 0;
+		try{
+			Scanner reader = new Scanner(new File("Random-tests-100-20.txt"));
+			int numOfTests = reader.nextInt();
+			int arraySize = reader.nextInt();
+			int key, result;
+			for(int i = 0; i < numOfTests; i++){
+				key = reader.nextInt();
+				int[] testArray = new int[arraySize];
+				for(int j = 0; j < arraySize; j++){
+					testArray[j] = reader.nextInt();
+				}
+				result = reader.nextInt();
+				try{
+					int testRes = alg.myContains5(testArray,key);
+					if(testRes == result){
+						//System.out.println(ANSI_GREEN+"Pass " + (i+1) +ANSI_RESET);
+						rightCounter++;
+					}else{
+						//System.out.println(ANSI_RED+"Fail " + (i+1) + ": Wrong answer"+ANSI_RESET);
+						wrongCounter++;
+					}
+				}catch(Exception a){
+					//System.out.println(ANSI_RED+"Fail " + (i+1) + ": Exception"+ANSI_RESET);
+					wrongCounter++;
+				}
+			}
+		}catch(FileNotFoundException e){
+			e.printStackTrace();
+		}
+
+		System.out.println("myContains5: Wrong on " + wrongCounter + " out of " + (wrongCounter+rightCounter));
+	}
+
+	public static void runTest6(){
+		Algorithm alg = new Algorithm();
+		int wrongCounter = 0;
+		int rightCounter = 0;
+		try{
+			Scanner reader = new Scanner(new File("Random-tests-100-20.txt"));
+			int numOfTests = reader.nextInt();
+			int arraySize = reader.nextInt();
+			int key, result;
+			for(int i = 0; i < numOfTests; i++){
+				key = reader.nextInt();
+				int[] testArray = new int[arraySize];
+				for(int j = 0; j < arraySize; j++){
+					testArray[j] = reader.nextInt();
+				}
+				result = reader.nextInt();
+				try{
+					int testRes = alg.myContains6(testArray,key);
+					if(testRes == result){
+						//System.out.println(ANSI_GREEN+"Pass " + (i+1) +ANSI_RESET);
+						rightCounter++;
+					}else{
+						//System.out.println(ANSI_RED+"Fail " + (i+1) + ": Wrong answer"+ANSI_RESET);
+						wrongCounter++;
+					}
+				}catch(Exception a){
+					//System.out.println(ANSI_RED+"Fail " + (i+1) + ": Exception"+ANSI_RESET);
+					wrongCounter++;
+				}
+			}
+		}catch(FileNotFoundException e){
+			e.printStackTrace();
+		}
+
+		System.out.println("myContains6: Wrong on " + wrongCounter + " out of " + (wrongCounter+rightCounter));
+	}
+
 }
