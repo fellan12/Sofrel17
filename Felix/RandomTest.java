8ec762bd4f5098feefad03c894b46b622ae80003
Fellan12 <fellan12@gmail.com>
2017-02-09 15:23:31 +0100

    qfix

---
+++ Felix/RandomTest.java
@@ -0,0 +1,78 @@
+import java.util.Random;
+import java.util.*;
+import java.io.*;
+import java.io.PrintWriter;
+import java.lang.StringBuilder;
+
+public class RandomTest{
+	static int[][] tests;
+	static int[] keys;
+	static int numOfTests = 100;
+	static int sizeOfArray = 20;
+	static int numberRange = 20;
+	public static void main(String[] args) {
+		tests = new int[numOfTests][sizeOfArray];
+		keys = new int[numOfTests];
+		String filename = "Random-tests-"+numOfTests+"-"+sizeOfArray+"-"+numberRange+".txt";
+
+		randomGenTests(numOfTests,sizeOfArray,numberRange,filename);
+		System.out.println("Generated " + numOfTests+ " tests");
+		System.out.println("Array size of " +sizeOfArray);
+		System.out.println("Number range of " +numberRange);
+		System.out.println("In the file: " + filename);
+		System.out.println();
+		System.out.println("Running test....");
+	}
+
+	public static void randomGenTests(int numOfTests, int sizeOfArray, int numberRange, String filename){
+		try{
+			PrintWriter writer = new PrintWriter(filename, "UTF-8");
+			writer.println(numOfTests);
+			writer.println(1);
+			writer.println(sizeOfArray);
+			writer.println(1);
+
+			Random rand = new Random();
+
+			
+			for(int i = 0; i < numOfTests; i++){
+				ArrayList<String> testArray = new ArrayList<String>();
+				StringBuilder sb = new StringBuilder();
+
+				for(int j = 0; j < sizeOfArray; j++){					//Generate the test array
+					int randNum = rand.nextInt(numberRange);
+					testArray.add(randNum + " ");		//Add a random number from 0 to numberRange-1
+					tests[i][j] = randNum;
+				}
+
+				int key = rand.nextInt(numberRange);					//Generate the key
+				keys[i] = key;
+
+				boolean keyInArray = testArray.contains(key+ " ");		//Check it the key is in the array
+
+				for(String x : testArray){								//Convert the Array to string for writing
+					sb.append(x);
+				}
+				System.out.println(key + " [" + sb.toString().trim() + "] " +keyInArray);
+				writer.println(key + " [" + sb.toString().trim() + "] " +keyInArray);	//Write the testcase to file
+			}
+
+			writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void runTest(){
+		Algorithm alg = new Algorithm();
+		boolean testResult;
+		for(int i = 0; i < tests.length; i++){
+			for(int j = 0; j < tests[0].length; j++){
+				ArrayList<Integer> testArray = new ArrayList<Integer>(Arrays.asList(tests[i]));
+				testResult = alg.myContains(array,keys[i]);
+
+
+			}
+		}
+	}
+}
